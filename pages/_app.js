c8cbc808bdf411c26de8c99eb8c0d913592532c6
Yifei Kong <kongyifei@gmail.com>
2020-09-15 14:25:17 +0800

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/animations/scale.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
